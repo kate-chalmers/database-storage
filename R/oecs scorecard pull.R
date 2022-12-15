3c27ea55f8ccd650fc4b8748c15c4542b3946716
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2022-12-15 15:28:35 +0100

    new browser

--- R/oecs scorecard pull.R
+++ R/oecs scorecard pull.R
@@ -26,9 +26,14 @@ current_year <- as.numeric(format(Sys.Date(), format="%Y")) - 1
 end_date <- paste0(current_year, "-12-31")
 
 # port_num <- round(runif(1, min=1, max=9999),0) %>% as.integer()
-port_num <- 4444L
-rD <- rsDriver(browser="firefox", port=port_num, verbose=F)
+port_num <- 4445L
+remDrtemp <- remoteDriver()
+rD <- rsDriver(browser=remDrtemp[["browserName"]], port=port_num, verbose=F)
+
+
+# rD <- rsDriver(browser="firefox", port=8007L, verbose=F)
 remDr <- rD[["client"]]
+# remDr <- remoteDriver()
 
 # Pull currency conversion
 
