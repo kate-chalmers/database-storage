d80117baba6ecd96d49c828454462695525715a3
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2023-01-05 17:02:58 +0100

    revert

--- R/script.R
+++ R/script.R
@@ -2,10 +2,10 @@ library(RSelenium)
 
 system("docker pull selenium/standalone-chrome", wait=TRUE)
 Sys.sleep(5)
-system("docker run -d -p 4449:4444 selenium/standalone-chrome", wait=TRUE)
+system("docker run -d -p 4445:4444 selenium/standalone-chrome", wait=TRUE)
 Sys.sleep(5)
 
-remDr <- remoteDriver("localhost", 4449L, "chrome")
+remDr <- remoteDriver("localhost", 4445L, "chrome")
 remDr$open()
 remDr$navigate("https://phptravels.com/demo")
 html <- remDr$getPageSource()
